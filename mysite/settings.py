3fc1bca188be499e74e508fb56c3e54c2fbd05d8
Austin Meyers <meyersaustin22@gmail.com>
2015-07-31 00:47:35 +0000

    Migrated to Mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'djangotest',
+		'USER': 'test',
+		'PASSWORD': 'poop',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
